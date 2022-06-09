b954aedfa957b59eed3a4c0c8905339df0da7a47
pudrikrete <89629247+pudrikrete@users.noreply.github.com>
2022-06-09 13:08:52 -0400

    Update start.ts
    Added code to support https; configured http by default.

--- server/src/start.ts
+++ server/src/start.ts
@@ -3,6 +3,17 @@ import app from '@server';
 // Start the server
 const port = Number(process.env.PORT || 3031);
 
+//var fs = require('fs');
+//var https = require('https');
+//var privateKey  = fs.readFileSync('src/certs/private.key', 'utf8');
+//var certificate = fs.readFileSync('src/certs/certificate.crt', 'utf8');
+//var credentials = {key: privateKey, cert: certificate};
+
+//var httpsServer = https.createServer(credentials, app);
+//httpsServer.listen(port, () => {
+//	console.log('HTTPS express server started on port: ' + port);
+//});
+
 app.listen(port, () => {
-    console.log('Express server started on port: ' + port);
+   console.log('Express server started on port: ' + port);
 });
